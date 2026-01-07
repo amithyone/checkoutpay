c1a0d5332f63d0b3c0ab9dc6fb58421f9d1ddf68
amithyone <amithyone@gmail.com>
2026-01-07 06:01:49 -0800

    Add Log facade import to SetupController

--- app/Http/Controllers/SetupController.php
+++ app/Http/Controllers/SetupController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Str;
 
 class SetupController extends Controller
